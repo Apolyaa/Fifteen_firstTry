d7d68e99f3a868214ad7ca613cab39324be00b3c
Apolya <polinagolubeva02@gmail.com>
2022-11-21 21:39:37 +0300

    add post request to save game data

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import com.sun.istack.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,4 +24,11 @@ public class TaskController {
     public String reward() {
         return "reward";
     }
+    @PostMapping("/saveGameData")
+    @ResponseStatus(HttpStatus.OK)
+    public void saveGameData(@RequestBody String request) {
+        Logger logger = LoggerFactory.getLogger(TaskController.class);
+        logger.info(request);
+    }
 }
+
--- src/main/resources/static/reward.js
+++ src/main/resources/static/reward.js
@@ -5,3 +5,34 @@ window.onload = function() {
     time.innerText = localStorage.getItem('time');
     clicks.innerText = localStorage.getItem('clicks');
 }
+
+function reqListener () {
+
+    if (this.status != 200) {
+        alert(`Ошибка ${this.status}: ${this.statusText}`);
+    } else {
+        window.location.href = "./";
+    }
+}
+
+function saveData(e) {
+
+    // 1. Создаём новый XMLHttpRequest-объект
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', './saveGameData');
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    xhr.onload = reqListener;
+
+    let json = JSON.stringify({
+        dateTime: new Date(),
+        user: 'userName',
+        gameTime: localStorage.getItem('time'),
+        clicks: localStorage.getItem('clicks'),
+    });
+
+    xhr.send(json);
+
+    return false;
+}
